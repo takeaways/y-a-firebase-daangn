452cb1b52bb71fce0791fb01096eded725fe2099
lucas.j <lucas.j@hpcnt.com>
2021-08-08 15:00:00 +0900

    end setting in github

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,5 @@
+import * as bootstrap from "bootstrap";
+import { myFirebase } from "./firebase";
+
 console.log("---");
+console.log(myFirebase);
---
+++ src/js/firebase/index.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
